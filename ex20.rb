b9da8f5535e69ffe4519e92658cd88e721c4953c
Dan Schmidt <daniel.adam.schmidt@gmail.com>
2012-01-03 04:16:06 -0600

    I find the first line a little confusing, I though the zsh element of ARGV was supposed to be the script name.

---
+++ ex20.rb
@@ -0,0 +1,36 @@
+input_file = ARGV[0]
+
+def print_all(f)
+  puts f.read()
+end
+
+def rewind(f)
+  f.seek(0, IO::SEEK_SET)
+end
+
+def print_a_line(line_count, f)
+  puts "#{line_count} #{f.readline()}"
+end
+
+current_file = File.open(input_file)
+
+puts "First, lets print the whole file:"
+puts # prints a blank line
+
+print_all(current_file)
+
+puts "Now let's rewind, kind of like a tape."
+
+rewind(current_file)
+
+puts "Let's print out three lines:"
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
