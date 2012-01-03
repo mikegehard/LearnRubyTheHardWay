9ac1e605faeffec9e41b2a3c72db33896c4769f4
Dan Schmidt <daniel.adam.schmidt@gmail.com>
2012-01-03 01:35:01 -0600

    Passing command-line arguments to variables; it's important to pass the correct number to your script.

---
+++ ex13.rb
@@ -0,0 +1,6 @@
+first, second, third = ARGV
+
+puts "The script is called: #{$0}"
+puts "Your first variable is #{first}"
+puts "Your second variable is #{second}"
+puts "Your third variable is #{third}"
