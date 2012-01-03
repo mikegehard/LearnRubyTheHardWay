8703f642e241341b6ec7cd08dee134add5908a4b
Dan Schmidt <daniel.adam.schmidt@gmail.com>
2012-01-03 01:03:49 -0600

    More printing with the introduction of string escape sequences.

---
+++ ex10.rb
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = <<MY_HEREDOC
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+MY_HEREDOC
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
