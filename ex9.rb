86536fa6079bf3c0aef977ddc66c2e86ee89add1
Dan Schmidt <daniel.adam.schmidt@gmail.com>
2012-01-03 00:56:58 -0600

    Even more printing.

---
+++ ex9.rb
@@ -0,0 +1,14 @@
+# Here's some new strange stuff, remember to print it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days:", days
+puts "Here are the months:", months
+
+puts <<PARAGRAPH
+There's something going on here.
+With the PARAGRAPH thing.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+PARAGRAPH
