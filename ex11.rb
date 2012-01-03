8a9f44a366f69972b0de5ad9e3c4cf07d8053ddb
Dan Schmidt <daniel.adam.schmidt@gmail.com>
2012-01-03 01:12:58 -0600

    Using gets.chomp() to prompt the user and manipulate STDIN/STDOUT

---
+++ ex11.rb
@@ -0,0 +1,8 @@
+print "How old are you? "
+age = gets.chomp()
+print "How tall are you? "
+height = gets.chomp()
+print "How much do you weight? "
+weight = gets.chomp()
+
+puts "So, you're #{age} years old, #{height} tall and #{weight} heavy."
