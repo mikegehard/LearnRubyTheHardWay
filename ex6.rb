c792ec1e7e850c96786dbf504d690157cae0bcd0
Dan Schmidt <daniel.adam.schmidt@gmail.com>
2012-01-02 23:53:55 -0600

    Continue to do string insertion/formatting; Introduced to a new data type: Booleans; Also use the addition operator to 'concatenate' two strings into one string.

---
+++ ex6.rb
@@ -0,0 +1,20 @@
+x = "There are #{10} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e
