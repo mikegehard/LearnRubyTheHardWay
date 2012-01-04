396356ba11eabdd80297384ca45f12e6d6b1eaee
Dan Schmidt <daniel.adam.schmidt@gmail.com>
2012-01-03 23:14:02 -0600

    Using arrays and for-loops to do things repetively and quickly.

---
+++ ex32.rb
@@ -0,0 +1,33 @@
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+# this first kind of for-loop goes through an array
+for number in the_count
+  puts "This is count #{number}"
+end
+
+# same as above, but using a block instead
+fruits.each do |fruit|
+  puts "A fruit of type: #{fruit}"
+end
+
+# also we can go through mixed arrays too
+for i in change
+  puts "I got #{i}"
+end
+
+# we can also build arrays, first start with an empty one
+elements = []
+
+# then use a range objectt to do 0 to 5 counts
+for i in (0..5)
+  puts "Adding #{i} to the list."
+  # push is a function that arrays understand
+  elements.push(i)
+end
+
+# now we can puts them out too
+for i in elements
+  puts "Element was: #{i}"
+end 
