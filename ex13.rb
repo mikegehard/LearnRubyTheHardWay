88a35060f5ba4fc1f68878791f6ff10d83b7c508
Dan Schmidt <daniel.adam.schmidt@gmail.com>
2012-01-07 12:43:50 -0600

    Cleaning up questions branch

--- ex13.rb
+++
@@ -1,6 +0,0 @@
-first, second, third = ARGV
-
-puts "The script is called: #{$0}"
-puts "Your first variable is #{first}"
-puts "Your second variable is #{second}"
-puts "Your third variable is #{third}"
